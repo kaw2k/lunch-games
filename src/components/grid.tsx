314bfb276e77f6f1309e5a3c8fa176a81a3d7eb8
rogorman9 <ryanogorman@WTA-MBP419.local>
2019-05-24 15:58:02 -0400

    Slims the grid columns a bit

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -6,7 +6,7 @@ interface Props {}
 const useStyles = makeStyles({
   root: {
     display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(100px, 1fr))',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(90px, 1fr))',
     gridGap: '1em',
   },
 })
