9bbce5c6bd24e1a5525f17c5a9ab332ae4ceded4
michalhus <michal.hus@willowtreeapps.com>
2019-08-05 08:17:48 -0400

    SH - after being killed user can spectated rest of the game.

--- src/games/secret-hitler/views/game/overview.tsx
+++ src/games/secret-hitler/views/game/overview.tsx
@@ -4,6 +4,8 @@ import { ActionRow } from '../../../../components/actionRow'
 import { Button } from '../../../../components/button'
 import { SecretHitlerGameContext } from '../../../../helpers/contexts'
 import { Typography } from '@material-ui/core'
+// import { setView, view } from './spectate'
+import { Spectate } from './spectate'
 
 interface Props {
   myTurn: () => void
@@ -14,11 +16,6 @@ export const Overview: React.SFC<Props> = ({ myTurn }) => {
 
   return (
     <>
-      {!player.living && (
-        <Typography align="center" gutterBottom variant="h2" color="error">
-          you are dead
-        </Typography>
-      )}
       {game.message && (
         <Typography align="center" gutterBottom variant="h1">
           {game.message}
@@ -27,7 +24,7 @@ export const Overview: React.SFC<Props> = ({ myTurn }) => {
       <Board game={game} />
 
       <ActionRow fixed>
-        {player.living && (
+        {player.living ? (
           <Button
             color="green"
             disabled={
@@ -35,6 +32,10 @@ export const Overview: React.SFC<Props> = ({ myTurn }) => {
             }
             onClick={myTurn}>
             my turn
+          </Button>
+        ) : (
+          <Button confirm color="yellow" onClick={() => alert('TESTING')}>
+            spectate
           </Button>
         )}
       </ActionRow>
--- src/games/secret-hitler/views/game/performPower/kill.tsx
+++ src/games/secret-hitler/views/game/performPower/kill.tsx
@@ -2,27 +2,35 @@ import * as React from 'react'
 import { SecretHitlerGameContext } from '../../../../../helpers/contexts'
 import { ChoosePlayers } from '../../../../../components/choosePlayers'
 import { values } from 'ramda'
+import { playerName } from '../../../../../components/playerName'
 
 export const Kill: React.SFC<{}> = () => {
-  const { endGame, updateGame, updateGamePlayer, game } = React.useContext(
-    SecretHitlerGameContext
-  )
+  const {
+    endGame,
+    updateGame,
+    updateGamePlayer,
+    game,
+    player,
+  } = React.useContext(SecretHitlerGameContext)
 
   return (
     <ChoosePlayers
       title="Who do you want to kill?"
       doneText="kill"
       players={values(game.players).filter(p => p.living)}
-      onDone={async ([player]) => {
-        const p = game.players[player.id]
+      onDone={async ([targetPlayer]) => {
+        const p = game.players[targetPlayer.id]
         if (p.role.isHitler) {
           endGame(
             'liberal',
-            `${p.name || p.name} was killed and is hitler, liberals win!`
+            `${playerName(p)} was killed and is hitler, liberals win!`
           )
         } else {
           updateGamePlayer({ ...p, living: false })
-          updateGame({ performPower: null })
+          updateGame({
+            performPower: null,
+            message: `${playerName(player)} killed ${playerName(p)}`,
+          })
         }
       }}
     />
